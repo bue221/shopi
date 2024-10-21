4ee675e88493674710773549c8ff39159fe50985
Andres Plaza (@bue221) <camiloplaza3@gmail.com>
2024-10-21 01:44:34 -0500

    Refactor: Add StrictMode to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes.config";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
